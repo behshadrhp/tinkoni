a10189a33a9e557b3f5f72f22a0bcc15a2c28e4d
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-08-10 16:24:47 +0330

    :zap:interview get method

--- app/main.py
+++ app/main.py
@@ -1,7 +1,37 @@
-from fastapi import FastAPI
+from enum import Enum
+from typing import Optional
 
-from app.routers.post import router as post_router
+from fastapi import FastAPI
 
 app = FastAPI()
 
-app.include_router(post_router, prefix="/post")
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome Back!"}
+
+
+@app.get("/blog/all/")
+async def get_blog(page: int = 1, page_size: Optional[int] = 2):
+    return {"message": f"all {page_size} of blog on pages {page}"}
+
+
+@app.get("/blog/{id}/comment/{comment_id}/")
+async def get_comment(id: int, comment_id: int, username: Optional[str] = None):
+    return {"message": f"blog_id: {id}, comment_id: {comment_id}, username: {username}"}
+
+
+class BlogType(str, Enum):
+    short = "short"
+    long = "long"
+    pain = "pain"
+
+
+@app.get("/blog/type/{type}")
+async def get_blog_type(type: BlogType):
+    return {"message": f"blog type is {type}"}
+
+
+@app.get("/blog/post/{id}")
+async def get_post(id: int):
+    return {"message": f"blog post is {id}"}
