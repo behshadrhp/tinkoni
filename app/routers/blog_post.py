484c637684580d4c0b428d106c48c93691e65fd5
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-08-19 23:32:18 +0330

    :memo: create BlogModel

--- app/routers/blog_post.py
+++ app/routers/blog_post.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/blog", tags=["blog"])
 
 
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    comments: int
+    published: bool = False
+
+
 @router.post("/create/")
-async def post_blog():
-    pass
+async def post_blog(blog: BlogModel):
+
+    return {"data": blog}
