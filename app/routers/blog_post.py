bd668ead654a76d41f803e36c71f797a73b59a0e
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-08-24 10:18:09 +0330

    :fire: close this project - last comment :)

--- app/routers/blog_post.py
+++ app/routers/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
 
 router = APIRouter(prefix="/blog", tags=["blog"])
@@ -11,7 +11,12 @@ class BlogModel(BaseModel):
     published: bool = False
 
 
-@router.post("/create/")
-async def post_blog(blog: BlogModel):
+@router.post("/create/{id}")
+async def post_blog(blog: BlogModel, id: int, version: int = 1):
 
-    return {"data": blog}
+    return {"id": id, "version": version, "data": blog}
+
+
+@router.post("/create/{id}/comment")
+async def comment_blog(blog: BlogModel, id: int, comment_id: int = Query(None)):
+    return {"id": id, "comment_id": comment_id, "blog": blog}
